fcd1ccb6fa41d3239ea28b68a8f75c1c5508a05d
Paul Ashour <paul.ashour@gmail.com>
2019-07-09 09:32:20 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,12 +17,32 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find_by_id(params[:id])
+  end
+
   def create
     Product.create(product_params)
     redirect_to products_path
   end
 
+  def data
+    render plain: serializeProduct(params[:id])
+  end
+
   private
+
+  def serializeProduct(id)
+    p = Product.find_by_id(id)
+    serial = '{' +
+      [
+        %Q("id": #{p.id}),
+        %Q("name": "#{p.name}"),
+        %Q("price": #{p.price}),
+        %Q("description": "#{p.description}"),
+        %Q("inventory": #{p.inventory || 0})
+      ].join(', ') + '}'
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
